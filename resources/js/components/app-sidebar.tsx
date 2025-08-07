06883c9166986426ac32eaf61064450dfc2b61ee
IannxGusion <ardiansyahrian771@gmail.com>
2025-08-07 10:15:12 +0700

    repository

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -38,7 +38,7 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/IannxGusion/LandingPageSCM.git',
         icon: Folder,
     },
 ];
